433b0b4a6196dc6c4bce2ebbccc90f9cf1978ae7
Sandro Filho <sandrofilho98@yahoo.com.br>
2021-11-13 17:35:04 -0300

    set window right fix

--- assets/cart.js
+++ assets/cart.js
@@ -3,25 +3,28 @@ $(function(){
     function setWindowRight(){
         var cartWidth = document.querySelector('section.cart').offsetWidth;
         $('section.cart').css('right', cartWidth * (-1))
-        $(".bx.bxs-shopping-bag, p.cartNumber, .backArrow").click(function(){
-            var cartWidth = document.querySelector('section.cart').offsetWidth;
-            $('section.cart').toggleClass("active")
-            if($( "section.cart" ).hasClass( "active" )){
-                $('html').css('right', cartWidth)
-            }else{
-                $('html').css('right', 0)
-            }
-            
-        })
     }
     setWindowRight()
+
+    window.addEventListener('resize', setWindowRight())
+
+    $(".bx.bxs-shopping-bag, p.cartNumber, .backArrow").click(function(){
+        var cartWidth = document.querySelector('section.cart').offsetWidth;
+        $('section.cart').toggleClass("active")
+        if($( "section.cart" ).hasClass( "active" )){
+            $('html').css('right', cartWidth)
+        }else{
+            $('html').css('right', 0)
+        }
+        
+    })
 
     $(".comprarSingle").click(function(){
         var cartQtnTotal = Number($('.cartNumber').html())
         $('.cartNumber').html(cartQtnTotal+=1)
     })
 
-    window.addEventListener('resize', setWindowRight())
+    
 
     
 })
