20168b80a305f86f7a1b69fe5b4fc8a219b3fc4d
Sandro Filho <sandrofilho98@yahoo.com.br>
2021-11-13 17:32:12 -0300

    set window right

--- assets/cart.js
+++ assets/cart.js
@@ -1,21 +1,26 @@
 $(function(){
-    var cartWidth = document.querySelector('section.cart').offsetWidth;
-    $('section.cart').css('right', cartWidth * (-1))
 
-    $(".bx.bxs-shopping-bag, p.cartNumber, .backArrow").click(function(){
+    function setWindowRight(){
         var cartWidth = document.querySelector('section.cart').offsetWidth;
-        $('section.cart').toggleClass("active")
-        if($( "section.cart" ).hasClass( "active" )){
-            $('html').css('right', cartWidth)
-        }else{
-            $('html').css('right', 0)
-        }
-        
-    })
+        $('section.cart').css('right', cartWidth * (-1))
+        $(".bx.bxs-shopping-bag, p.cartNumber, .backArrow").click(function(){
+            var cartWidth = document.querySelector('section.cart').offsetWidth;
+            $('section.cart').toggleClass("active")
+            if($( "section.cart" ).hasClass( "active" )){
+                $('html').css('right', cartWidth)
+            }else{
+                $('html').css('right', 0)
+            }
+            
+        })
+        $(".comprarSingle").click(function(){
+            var cartQtnTotal = Number($('.cartNumber').html())
+            $('.cartNumber').html(cartQtnTotal+=1)
+        })
+    }
+    setWindowRight()
 
+    window.addEventListener('resize', setWindowRight())
 
-    $(".comprarSingle").click(function(){
-        var cartQtnTotal = Number($('.cartNumber').html())
-        $('.cartNumber').html(cartQtnTotal+=1)
-    })
+    
 })
